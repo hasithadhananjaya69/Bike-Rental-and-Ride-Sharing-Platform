2fb1fef584ca0daf2c7d11cee26e4576a6a256c9
hasithadhananjaya69 <anjanadhananjaya100@gmail.com>
2026-03-15 15:25:53 +0530

    Revert "add comment for this code"
    This reverts commit 58d3e314500a73a69568ef10e3913eaa53b52f6e.

--- Rider Driver managment/src/main/java/ridedrivermanagement/controller/DriverController.java
+++ Rider Driver managment/src/main/java/ridedrivermanagement/controller/DriverController.java
@@ -17,7 +17,7 @@ public class DriverController {
         this.driverService = driverService;
     }
 
-    // READ: View All Drivers details
+    // READ: View All Drivers
     @GetMapping("/dashboard")
     public List<Driver> getAllDrivers() {
         return driverService.getAllDrivers();
