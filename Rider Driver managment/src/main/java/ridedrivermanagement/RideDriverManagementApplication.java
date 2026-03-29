eb6a988219299238567840579d3aef00512eb8df
hasithadhananjaya69 <anjanadhananjaya100@gmail.com>
2026-03-29 21:32:33 +0530

    rebuild

--- Rider Driver managment/src/main/java/ridedrivermanagement/RideDriverManagementApplication.java
+++ Rider Driver managment/src/main/java/ridedrivermanagement/RideDriverManagementApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RideDriverManagementApplication {
+public class    RideDriverManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RideDriverManagementApplication.class, args);
